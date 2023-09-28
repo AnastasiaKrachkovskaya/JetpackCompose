767d99ee6ff6ade9c1a159eab74da874993b9267
Анастасия Крачковская <krachkovskaya4@mail.ru>
2023-09-29 00:40:50 +0300

    Приложение погоды на jetpack compose

---
+++ app/src/main/java/com/example/weathercompose/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.weathercompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.weathercompose.screens.MainCard
+import com.example.weathercompose.screens.TabLayout
+import com.example.weathercompose.ui.theme.WeatherComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherComposeTheme {
+                Image(
+                    painter = painterResource(id = R.drawable.background),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.5f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+                }
+
+
+            }
+        }
+    }
+}
+
